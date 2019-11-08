6d8113bf04e3300761008ba7069458ed47699d1a
Mario Diaz Justicia <mdiazjusticia@gmail.com>
2019-11-08 17:14:18 +0100

    inmobi 9.0.1

---
+++ Scopely-InMobi/9.0.1/InMobiAdapter/InMobiAdapterConfiguration.h
@@ -0,0 +1,30 @@
+//
+//  InMobiAdapterConfiguration.h
+//  InMobiMoPubSampleApp
+//
+//  Created by Iti Agrawal on 11/09/19.
+//
+
+#import <Foundation/Foundation.h>
+#if __has_include(<MoPub/MoPub.h>)
+#import <MoPub/MoPub.h>
+#elif __has_include(<MoPubSDKFramework/MoPub.h>)
+#import <MoPubSDKFramework/MoPub.h>
+#else
+#import "MPBaseAdapterConfiguration.h"
+#endif
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InMobiAdapterConfiguration : MPBaseAdapterConfiguration
+
+@property (nonatomic, copy, readonly) NSString * adapterVersion;
+@property (nonatomic, copy, readonly) NSString * biddingToken;
+@property (nonatomic, copy, readonly) NSString * moPubNetworkName;
+@property (nonatomic, copy, readonly) NSString * networkSdkVersion;
+
+- (void)initializeNetworkWithConfiguration:(NSDictionary<NSString *, id> * _Nullable)configuration complete:(void(^ _Nullable)(NSError * _Nullable))complete;
+
+@end
+
+NS_ASSUME_NONNULL_END
